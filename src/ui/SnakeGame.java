e7c9a2df450000a7f22b65f93d4f5f42c8266ea6
Nicolas Penagos Montoya <nicolas.penagosm98@gmail.com>
2020-05-09 00:33:05 -0500

    First commit

---
+++ src/ui/SnakeGame.java
@@ -0,0 +1,24 @@
+package ui;
+
+import processing.core.PApplet;
+
+public class SnakeGame extends PApplet{
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		PApplet.main("ui.SnakeGame");
+	}
+	
+
+	// -------------------------------------
+	// Processing Methods
+	// -------------------------------------
+	
+	public void settings() {
+
+		size(600, 600);
+
+	}
+
+
+}
